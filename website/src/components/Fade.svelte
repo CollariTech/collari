3362b3a22e6c491553bce060d8842667d345cde4
neston <134344306+holasoycezar@users.noreply.github.com>
2024-11-01 00:27:19 -0300

    feat(core): integrate Svelte for hero section with custom styles  - Added AOS animations to hero - Applied new color scheme to hero section and adjusted styles for modern look - Set up Svelte component integration in Astro project for reactivity and modularity

---
+++ website/src/components/Fade.svelte
@@ -0,0 +1,9 @@
+<!-- ainda vou pensar na necessidade disso e transportar as animações pra dentro dos headers  -->
+<script>
+    import { fade } from 'svelte/transition';
+    export let delay = 0;
+  </script>
+  
+  <div transition:fade={{ delay }}>
+    <slot></slot>
+  </div>
